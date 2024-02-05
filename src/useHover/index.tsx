da8f62ed3bb0987272722d64514d541a17695270
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-02-05 09:33:48 +0500

    sggrr

--- src/useHover/index.tsx
+++ src/useHover/index.tsx
@@ -13,15 +13,15 @@ const App5 = (): ReactElement => {
   const [ref, hovering] = useHover();
   const Ref = ref as RefObject<HTMLElement>;
 
-  const backgroundColor = hovering ? ` background-color:${getRandomColor()}` : 'background-color:charcoal';
+  const backgroundColor = hovering ? getRandomColor() : 'charcoal';
 
   return (
-    <section className="grid h-[350px] w-[350px] place-items-center">
+    <section className="grid h-[350px] w-[350px] place-items-center border-black">
       <h1>useHover</h1>
       <article
         ref={Ref}
         style={{ backgroundColor }}
-        className="grid h-[150px] w-[150px] place-items-center bg-slate-500"
+        className="grid h-[150px] w-[150px] place-items-center rounded-2xl rounded-sm border-slate-950 bg-slate-500 hover:cursor-grab"
       >
         Hovering? {hovering ? 'Yes' : 'No'}
       </article>
